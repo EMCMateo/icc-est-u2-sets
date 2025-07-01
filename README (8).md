f459023eed2a593bb6a2e90f6db605251e2f6763
EMCMateo <mateo3456moli@gmail.com>
2025-07-01 08:54:44 -0500

    first commit

---
+++ README (8).md
@@ -0,0 +1,97 @@
+# Colecciones - SETS - Práctica de Estructuras Avanzadas
+
+## 📌 Información General
+
+- **Título:** Implementación de HashSets
+- **Asignatura:** Estructura de Datos
+- **Carrera:** Computación
+- **Estudiante:** Mateo Molina
+- **Fecha:** 1/07/2025
+- **Profesor:** Ing. Pablo Torres
+
+---
+
+## 🛠️ Descripción
+
+Este proyecto implementa LOS DISTINTOS METODOS SETS.
+
+- Usar todos los metodos Sets
+
+La lógica principal se encuentra en [`materia.controllers.ArbolBinario`](src/controllers/Sets.java) y se ejecuta en (src/app.java)
+
+---
+
+## 🚀 Ejecución
+
+Para compilar y ejecutar el proyecto desde la terminal:
+
+```sh
+javac -d bin src/App.java src/materia/controllers/ArbolBinario.java src/materia/models/Nodo.java
+java -cp bin App
+```
+
+---
+
+## 🧑‍💻 Ejemplo de Salida
+
+
+![alt text](image.png)
+
+
+```plaintext
+-------------------MATEO EDUARDO MOLINA CHAMBA----------------------
+-------------------HashSET----------------------
+Elemetnos del HashSet: 
+manzana
+pera
+celular
+laptop
+-----------------LinkedHashSet---------------------
+Elemetnos del LnkedHashSet: 
+laptop
+manzana
+pera
+celular
+-----------------TreeSet---------------------
+Elemetnos del TreeSet: 
+celular
+laptop
+manzana
+pera
+-----------------TreeSetComparacion---------------------
+Elemetnos del TreeSet con Comparador: 
+pera
+laptop
+celular
+celulas
+manzana
+-----------------TreeSetComparacionAlfabetico---------------------
+Elemetnos del TreeSet con Comparador Alfabeticamente: 
+manzana
+celulas
+celular
+laptop
+pera
+PS C:\Users\Mateo Molina\EstrDeDatos\icc-est-u2-sets> 
+```
+
+---
+
+## 📂 Estructura del Proyecto
+
+```
+src/
+  App.java
+    controllers/
+      Sets.java
+    
+```
+
+---
+
+## 📄 Notas
+
+- El proyecto está configurado para usar la carpeta `src` como fuente y `bin` como salida de compilación.
+- No requiere librerías externas.
+
+---
---
+++ src/App.java
@@ -0,0 +1,66 @@
+
+import controller.Sets;
+import java.util.*;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Sets sts = new Sets();
+        System.out.println("-------------------MATEO EDUARDO MOLINA CHAMBA----------------------");
+        System.out.println("-------------------HashSET----------------------");
+        runHashSet(sts);
+        System.out.println("-----------------LinkedHashSet---------------------");
+        runLinkedHashSet(sts);
+        System.out.println("-----------------TreeSet---------------------");
+        runTreeSet(sts);
+        System.out.println("-----------------TreeSetComparacion---------------------");
+        runTreeSetComparador(sts);
+        System.out.println("-----------------TreeSetComparacionAlfabetico---------------------");
+        runTreeSetComparadorAlfabetico(sts);
+    }
+    
+    public static void runHashSet (Sets set){
+        System.out.println(("Elemetnos del HashSet: "));
+        Set<String> elementos = set.construirHashSet();
+        for (String elemento : elementos){
+            System.out.println(elemento);
+        }
+
+    }
+
+    public static void runLinkedHashSet (Sets set){
+        System.out.println(("Elemetnos del LnkedHashSet: "));
+        Set<String> elementos = set.construirLinkedHashSet();
+        for (String elemento : elementos){
+            System.out.println(elemento);
+        }
+
+    }
+    public static void runTreeSet (Sets set){
+        System.out.println(("Elemetnos del TreeSet: "));
+        Set<String> elementos = set.construirTreeSet();
+        for (String elemento : elementos){
+            System.out.println(elemento);
+        }
+
+    }
+     public static void runTreeSetComparador (Sets set){
+        System.out.println(("Elemetnos del TreeSet con Comparador: "));
+        Set<String> elementos = set.construirTreeSetConComparador();
+        for (String elemento : elementos){
+            System.out.println(elemento);
+        }
+
+    }
+
+    public static void runTreeSetComparadorAlfabetico (Sets set){
+        System.out.println(("Elemetnos del TreeSet con Comparador Alfabeticamente: "));
+        Set<String> elementos = set.construirTreeSetConComparadorAlfabetico();
+        for (String elemento : elementos){
+            System.out.println(elemento);
+        }
+
+    }
+}
+
+
+
